f0d9cde98310439a846079639c38b5309495c781
packapun <packapun.dev@gmail.com>
2025-06-13 22:42:33 -0400

    Run bundle

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1749867364',resources: [
+    cacheName: 'chirpy-1749868931',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
